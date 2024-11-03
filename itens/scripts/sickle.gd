0bc3b3b8ae7f74f06edcfbe1ce87c7b734c17111
marianaselene <mariana02301@gmail.com>
2024-11-03 01:27:44 -0300

    V3.1

--- itens/scripts/sickle.gd
+++ itens/scripts/sickle.gd
@@ -1,4 +1,12 @@
 extends Area2D
+class_name Sickle
 
-func _ready() -> void:
-	pass # Replace with function body.
+signal item_picked_up
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	add_to_group("collectible")
+
+func _on_body_entered(body: Node2D):
+		queue_free()
+		print("Sickle coletado")
